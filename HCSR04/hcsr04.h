ebfe0565f9213722b62d25a86ecd04024811abb8
Fatih Safa Özgür <90393344+fsafaozgur@users.noreply.github.com>
2025-05-23 23:13:13 +0300

    sec

--- HCSR04/hcsr04.h
+++ HCSR04/hcsr04.h
@@ -2,7 +2,7 @@
 #define __HCSR04_H
 
   void HCSR04_Init(void);
-	uint16_t read_distance(void);
-	void wait_us(uint16_t delay);
+  uint16_t read_distance(void);
+  void wait_us(uint16_t delay);
 
 #endif /* __HCSR04_H */
