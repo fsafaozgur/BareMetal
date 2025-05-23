26a7758d6b6393cca2d8bbadd77a38c3f9924624
Fatih Safa Özgür <90393344+fsafaozgur@users.noreply.github.com>
2025-05-23 23:12:28 +0300

    corr

--- HCSR04/hcsr04.c
+++ HCSR04/hcsr04.c
@@ -4,24 +4,24 @@
 void HCSR04_Init(void)
 {
 	//trigger configuration
-		RCC->AHB2ENR |= 1u;
+	RCC->AHB2ENR |= 1u;
 	
-		GPIOA->MODER &= ~(3u << 2);
-		GPIOA->MODER |= (1u<<2);
-		GPIOA ->OTYPER &= ~ (1u<<1);
-    GPIOA->OSPEEDR &= ~(3u << 2);
-    GPIOA->PUPDR &= ~(3u << 2);
-
-		//echo configuration
-		GPIOA->MODER &= ~(3u << 4);
-    GPIOA->OSPEEDR &= ~(3u << 4);
-    GPIOA->PUPDR &= ~(3u << 4);
-
-    //timer configuration
-    RCC->APB1ENR1 |= (1<<4);
-    TIM6->PSC = 79;
-    TIM6->ARR = 10;
-    TIM6->CNT = 0;
+	GPIOA->MODER &= ~(3u << 2);
+	GPIOA->MODER |= (1u<<2);
+	GPIOA ->OTYPER &= ~ (1u<<1);
+	GPIOA->OSPEEDR &= ~(3u << 2);
+	GPIOA->PUPDR &= ~(3u << 2);
+	
+	//echo configuration
+	GPIOA->MODER &= ~(3u << 4);
+	GPIOA->OSPEEDR &= ~(3u << 4);
+	GPIOA->PUPDR &= ~(3u << 4);
+	
+	//timer configuration
+	RCC->APB1ENR1 |= (1<<4);
+	TIM6->PSC = 79;
+	TIM6->ARR = 10;
+	TIM6->CNT = 0;
 }
 
 
@@ -45,7 +45,7 @@ uint16_t read_distance(void)
 void wait_us(uint16_t delay)
 {
 	uint16_t current = TIM6->CNT;
-    TIM6->CR1 |= 1;
+    	TIM6->CR1 |= 1;
 	while ((TIM6->CNT - current) < delay);
 	TIM6->CR1 &= ~(1<<0);
 	TIM6->CNT = 0;
