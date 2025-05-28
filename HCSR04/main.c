e5eb7b7754d48c44028eb440fcec5073f5118409
Fatih Safa Özgür <90393344+fsafaozgur@users.noreply.github.com>
2025-05-28 23:13:59 +0300

    delay

--- HCSR04/main.c
+++ HCSR04/main.c
@@ -10,13 +10,12 @@ int main(void)
 
   while (1)
   {
-		GPIOA->ODR |= (1<<1);				//trigger pin set
-		wait_us(750);							//10us 
-		GPIOA->ODR &= ~(1<<1);			//trigger pin reset
+	GPIOA->ODR |= (1<<1);		//trigger pin set
+	wait_us(10);			//10us 
+	GPIOA->ODR &= ~(1<<1);		//trigger pin reset
 
-		distance = read_distance();	//read distance
-		
-		wait_us(3000);							//wait 
+	distance = read_distance();	//read distance
+	wait_us(3000);			//wait 
   }
 
 }
